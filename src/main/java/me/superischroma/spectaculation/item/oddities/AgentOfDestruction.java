3c31fba6a75d5a14dd4bfd2e5f1900ab48434c87
Furyloxx <161728619+Furyloxx@users.noreply.github.com>
2024-05-28 16:36:30 +0530

    Update AgentOfDestruction.java

--- src/main/java/me/superischroma/spectaculation/item/oddities/AgentOfDestruction.java
+++ src/main/java/me/superischroma/spectaculation/item/oddities/AgentOfDestruction.java
@@ -23,16 +23,19 @@ return "The &cGreat Bulla Cyclone The rain that stung like a thousand needles, "
 "Thousands of homes where swept away with the water, "+
 "Millions of people died near the coast, "+
 " The Most Dangerous Cyclone know as the &cGreat Bulla Cyclone.";
+ 
 @Override 
 public GenericItemType getType() 
 {  
 return GenericItemType.ITEM;
+}
 @Override
 public boolean isStackable() 
 {
  return false;  
 }  
-@Override  public String getURL() 
+@Override 
+public String getURL() 
 { 
 return "b543bbd96571b1e35aa039a5ebad65f41427c8b8871ddf767580f3a5b1622fbf"; 
 }
