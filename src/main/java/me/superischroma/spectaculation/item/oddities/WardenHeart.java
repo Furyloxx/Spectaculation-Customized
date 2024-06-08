32bef0db826ddb2b2c4ae0cbe0d3d12f2f03a5e4
Furyloxx <161728619+Furyloxx@users.noreply.github.com>
2024-06-08 12:57:12 +0530

    Added Warden Heart

---
+++ src/main/java/me/superischroma/spectaculation/item/oddities/WardenHeart.java
@@ -0,0 +1,47 @@
+package me.superischroma.spectaculation.item.oddities;
+
+import me.superischroma.spectaculation.item.GenericItemType;
+import me.superischroma.spectaculation.item.MaterialFunction;
+import me.superischroma.spectaculation.item.MaterialStatistics;
+import me.superischroma.spectaculation.item.Rarity;
+import me.superischroma.spectaculation.item.SkullStatistics;
+
+public class WardenHeart
+implements MaterialStatistics,
+MaterialFunction,
+SkullStatistics {
+    @Override
+    public String getDisplayName() {
+        return "Warden Heart";
+    }
+
+    @Override
+    public Rarity getRarity() {
+        return Rarity.LEGENDARY;
+    }
+
+    @Override
+    public GenericItemType getType() {
+        return GenericItemType.ITEM;
+    }
+
+    @Override
+    public boolean isEnchanted() {
+        return true;
+    }
+
+    @Override
+    public String getURL() {
+        return "19a3b5fab01dfabf1f8a814440a8ce80de109b135a51aef225c9ee98fe447e0";
+    }
+
+    @Override
+    public boolean isStackable() {
+        return false;
+    }
+
+    @Override
+    public String getLore() {
+        return "The heart of a powerful creature, dropped by the Atoned Horror.";
+    }
+}
