0bdcbc950f55db9576600a0b52af2ce06c3cb840
Furyloxx <161728619+Furyloxx@users.noreply.github.com>
2024-06-09 16:35:59 +0530

    Update accessory

---
+++ src/main/java/me/superischroma/spectaculation/item/accessory/MelodysHair.java
@@ -0,0 +1,5 @@
+package me.superischroma.spectaculation.item.accessory;
+
+import me.superischroma.spectaculation.item.*;
+
+public class SuperspeedTalisman implements AccessoryStatistics, MaterialFunction
