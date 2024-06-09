9f8167a629b755f0e67ed394a18a9e64b0bdea71
Furyloxx <161728619+Furyloxx@users.noreply.github.com>
2024-06-09 16:42:01 +0530

    Update MelodysHair.java

--- src/main/java/me/superischroma/spectaculation/item/accessory/MelodysHair.java
+++ src/main/java/me/superischroma/spectaculation/item/accessory/MelodysHair.java
@@ -3,3 +3,13 @@ package me.superischroma.spectaculation.item.accessory;
 import me.superischroma.spectaculation.item.*;
 
 public class SuperspeedTalisman implements AccessoryStatistics, MaterialFunction
+
+{
+  @Override
+  public int getDisplayName()
+  {
+    return "&dMelody's Hair'
+  }
+  public String Rarity getRarity()
+  {
+    return Rarity.EPIC
