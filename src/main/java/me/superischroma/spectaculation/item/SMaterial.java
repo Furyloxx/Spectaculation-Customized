99aa6dff9db428bb58afadff0eb3c7f9ee2256f6
Furyloxx <161728619+Furyloxx@users.noreply.github.com>
2024-06-10 19:30:17 +0530

    Update SMaterial.java

--- src/main/java/me/superischroma/spectaculation/item/SMaterial.java
+++ src/main/java/me/superischroma/spectaculation/item/SMaterial.java
@@ -87,6 +87,7 @@ public enum SMaterial
     // Dragon Assets
     WARDEN_HEART(Material.SKULL_ITEM, WardenHeart.class),
     WARDEN_HELMET(Material.SKULL_ITEM, WardenHelmet.class),
+    NECRONS_HANDLE(Material.STICK, NecronHandle.class),
     SUMMONING_EYE(Material.SKULL_ITEM, SummoningEye.class),
     SLEEPING_EYE(Material.SKULL_ITEM, SleepingEye.class),
     REMNANT_OF_THE_EYE(Material.SKULL_ITEM, RemnantOfTheEye.class),
