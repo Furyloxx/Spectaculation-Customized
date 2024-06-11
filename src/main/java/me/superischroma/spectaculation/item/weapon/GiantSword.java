cbf54be37079587440818cf5c44a46ed3b671195
Furyloxx <161728619+Furyloxx@users.noreply.github.com>
2024-06-11 08:16:45 +0530

    Update GiantSword.java

--- src/main/java/me/superischroma/spectaculation/item/weapon/GiantSword.java
+++ src/main/java/me/superischroma/spectaculation/item/weapon/GiantSword.java
@@ -1,6 +1,13 @@
 package me.superischroma.spectaculation.item.weapon;
 
 import me.superischroma.spectaculation.item.*;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.entity.*;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class GiantSword
 implements ToolStatistics,
@@ -45,3 +52,41 @@ Ability {
     public String getAbilityDescription() {
         return "Slam your sword into the ground dealing " + ChatColor.RED + "100,000 " + ChatColor.GRAY + "damage to nearby enemies.";
     }
+
+    @Override
+    public void ability(int action, SkyblockPlayer sbPlayer) {
+       if(action != INTERACT_RIGHT_CLICK) return;
+        if(!sbPlayer.manaCheck(100, "Giant's Slam")) return;
+
+        Player player = sbPlayer.getBukkitPlayer();
+
+        Block block = player.getTargetBlock(null, 6);
+        World world = block.getWorld();
+
+        Location loc = block.getLocation().subtract(0, 1, 0);
+        LivingEntity giant = (LivingEntity) world.spawnEntity(Utility.floor(loc), EntityType.GIANT);
+        LivingEntity ride = (LivingEntity) world.spawnEntity(Utility.floor(loc), EntityType.ARMOR_STAND);
+
+        ride.setMetadata("isNotSkyblockEntity", new FixedMetadataValue(SkyblockSandbox.getInstance(), true));
+        ride.setInvisible(true);
+        ride.setGravity(false);
+        ride.setInvulnerable(true);
+        ride.addPassenger(giant);
+
+        giant.setMetadata("isNotSkyblockEntity", new FixedMetadataValue(SkyblockSandbox.getInstance(), true));
+        giant.setCustomName("Dinnerbone");
+        giant.setCustomNameVisible(false);
+        giant.setGravity(false);
+        giant.setInvulnerable(true);
+        giant.setInvisible(true);
+
+        Objects.requireNonNull(giant.getEquipment()).setItemInMainHand(create());
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                ride.setHealth(0);
+                ride.remove();
+
+                giant.remove();
+                giant.setHealth(0);
+            }
