23e82a3237ca37ac1fbfb57af4ff837d0cfa05bb
Furyloxx <161728619+Furyloxx@users.noreply.github.com>
2024-06-22 13:51:35 +0530

    Update Humble.java

--- src/main/java/me/superischroma/spectaculation/item/oddities/Humble.java
+++ src/main/java/me/superischroma/spectaculation/item/oddities/Humble.java
@@ -3,3 +3,27 @@ package me.superischroma.spectaculation.item.oddities;
 import me.superischroma.spectaculation.item.*;
 
 public class QuiverArrow implements MaterialStatistics, MaterialFunction
+{
+    @Override
+    public String getDisplayName()
+    {
+        return ChatColor.DARK_GRAY + "TheHumbleguy's Balls";
+    }
+
+    @Override
+    public Rarity getRarity()
+    {
+        return Rarity.;
+    }
+
+    @Override
+    public GenericItemType getType()
+    {
+        return GenericItemType.ITEM;
+    }
+
+    @Override
+    public String getLore()
+    {
+        return "This item is in your inventory because you are holding your bow currently. Switch your held item to see the item that was here before.";
+    }
